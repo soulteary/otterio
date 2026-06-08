8f4155d97995ef97b49777006b2a62b6e5e78e0b
luojiyin <luojiyin@hotmail.com>
2026-06-08 22:27:04 +0800

    fix(cmd/fallocate_linux.go): correct spelling errors

--- cmd/fallocate_linux.go
+++ cmd/fallocate_linux.go
@@ -24,7 +24,7 @@ import "syscall"
 
 // Fallocate uses the linux Fallocate syscall, which helps us to be
 // sure that subsequent writes on a file just created will not fail,
-// in addition, file allocation will be contigous on the disk
+// in addition, file allocation will be contiguous on the disk
 func Fallocate(fd int, offset int64, length int64) error {
 	// No need to attempt fallocate for 0 length.
 	if length == 0 {
